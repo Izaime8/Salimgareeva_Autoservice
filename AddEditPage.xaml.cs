fda709f50367a82230948a781c3601a32641995d
prodUpal <tim.ism06@gmail.com>
2024-10-16 10:35:46 +0500

    6 лаба

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -20,9 +20,47 @@ namespace Salimgareeva_Autoservice
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Service _currentService = new Service();
+
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+
+            if (SelectedService != null)
+                _currentService = SelectedService;
+
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Укажите название услуги");
+            if (_currentService.Cost <= 0) //стоимость меньше нуля?
+                errors.AppendLine("Укажите стоимость услуги");
+            if (_currentService.Discount < 0 || _currentService.Discount > 1 || !_currentService.Discount.HasValue )
+                errors.AppendLine("Скидка введена некорректно");
+            if (string.IsNullOrWhiteSpace(_currentService.Duration))
+                errors.AppendLine("Укажите длительность усуги");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentService.ID == 0)
+                Salimgareeva_AutoserviceEntities.GetContext().Service.Add(_currentService);
+            try
+            {
+                Salimgareeva_AutoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -94,5 +94,24 @@ namespace Salimgareeva_Autoservice
         {
             UpdateServices();
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Salimgareeva_AutoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Salimgareeva_AutoserviceEntities.GetContext().Service.ToList();
+            }
+        }
     }
 }
